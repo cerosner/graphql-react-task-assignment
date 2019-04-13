18be060596df62a6fe7e5f9c7b3824cfb6a5f301
Cassie Rosner <cerosner@gmail.com>
2019-04-12 23:09:33 -0400

    initializes graphql schema and resolvers

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const PORT = 8080
+const graphqlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) => {
-  res.send('Hello world!')
-})
+app.use('/graphql', graphqlHttp({
+  // define type requirements then initialize into valid schema
+  schema: buildSchema(`
+    type RootQuery {
+      tasks: [String!]!
+    }
 
-app.listen(3000, () => {
+    type RootMutation {
+      createTask(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  // resolver functions obj that points to different endpoints
+  rootValue: {
+    // query type - GET
+    tasks: () => {
+      return ['cook', 'clean']
+    },
+    // mutation type - POST/PUT/DELETE
+    createTask: (args) => {
+      return args.name
+    }
+  },
+  // turn on user interface to check routes
+  graphiql: true
+}))
+
+app.listen(PORT, () => {
   console.log(`> I'm listening (:`)
-  console.log('> http://localhost:3000')
+  console.log(`> http://localhost:${PORT}/graphql`)
 })
