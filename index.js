d4b6ff13f263d666a609c2f7fba140992c6b48cc
Cassie Rosner <cerosner@gmail.com>
2019-04-19 20:05:50 -0400

    adds thorough documentation

--- index.js
+++ index.js
@@ -1,22 +1,90 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const PORT = 8080
+
+// GraphQL is a typed language
+
 const graphqlHttp = require('express-graphql')
+/*
+  middleware that routes requests through GraphQL
+  query parser '/graphql' endpoint to be handled
+  by the schemas that we define to be forwarded
+  to the associated resolver endpoints that we create
+
+  takes an obj with 3 properties to configure apis
+    - schema    // takes buildSchema()
+    - rootValue // obj of resolver functions
+    - graphiql  // optional UI to test apis @ '/graphql'
+*/
+
 const { buildSchema } = require('graphql')
+/*
+  parses and converts data to be used by
+  middleware that takes a `multi-line template
+  literal string` that defines our schemas
+
+  must adhere to GraphQL command specifications that are
+  looking for certain keywords
+
+  --- BOILERPLATE ---
+
+    graphqlHttp({
+      schema: buildSchema(`
+        type RootQuery {
+          data: [typeof data!]! // ! = not null
+        }
+
+        type RootMutation { // methods
+          addData(param: typeof param): typeof data
+          updateData(): ...
+          deleteData(): ...
+        }
+
+        schema {
+          query: fetch data from RootQuery
+          mutation: change data from RootMutation
+        }
+      `),
+      rootValue: {
+        // must use same names found in schema definition
+        data: () => {
+          return data
+        },
+        addData: (args) => {  // args carries obj of all params
+          store args.param
+        }
+      },
+      graphiql: boolean
+    })
+
+*/
 
 const app = express()
+
+// placeholder db
+const tasks = []
 
 app.use(bodyParser.json())
 
 app.use('/graphql', graphqlHttp({
-  // define type requirements then initialize into valid schema
   schema: buildSchema(`
+    type Task {
+      id: String!
+      title: String!
+      description: String!
+    }
+
+    input TaskInput {
+      title: String!
+      description: String!
+    }
+
     type RootQuery {
-      tasks: [String!]!
+      tasks: [Task!]!
     }
 
     type RootMutation {
-      createTask(name: String): String
+      createTask(taskInput: TaskInput): Task
     }
 
     schema {
@@ -24,18 +92,19 @@ app.use('/graphql', graphqlHttp({
       mutation: RootMutation
     }
   `),
-  // resolver functions obj that points to different endpoints
   rootValue: {
-    // query type - GET
     tasks: () => {
-      return ['cook', 'clean']
+      return tasks
     },
-    // mutation type - POST/PUT/DELETE
     createTask: (args) => {
-      return args.name
+      const task = {
+        id: Math.random().toString(),
+        title: args.title,
+        description: args.description
+      }
+      tasks.push(task)
     }
   },
-  // turn on user interface to check routes
   graphiql: true
 }))
 
