853ba3872b9b0c22c7e9329ee1cbab2400ce6701
Cassie Rosner <cerosner@gmail.com>
2019-06-06 21:07:10 -0400

    creates associations between tasks and users

--- database/index.js
+++ database/index.js
@@ -1,7 +1,12 @@
 const db = require('./db')
-const Tasks = require('./models/Task')
+const Tasks = require('./models/task')
+const Users = require('./models/user')
+
+Users.hasMany(Tasks)
+Tasks.belongsTo(Users)
 
 module.exports = {
   db,
-  Tasks
+  Tasks,
+  Users
 }
